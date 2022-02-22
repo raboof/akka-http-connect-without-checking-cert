9ce87707db4a1474bbf14e21263d46799e621115
Arnout Engelen <arnout@bzzt.net>
2022-02-22 17:41:14 +0100

    reproducer from stackoverflow

--- Main.scala
+++ Main.scala
@@ -2,35 +2,36 @@ import java.security.SecureRandom
 import java.security.cert.X509Certificate
 import javax.net.ssl._
 
-import scala.concurrent.Await
+import scala.concurrent._
 import scala.concurrent.duration._
 
-import akka.actor._
+import akka.actor.typed._
+import akka.actor.typed.scaladsl._
 import akka.http.scaladsl._
 import akka.http.scaladsl.client.RequestBuilding.Get
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.scaladsl._
 
 object Main extends App {
+  implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "SingleRequest")
+  implicit val executionContext: ExecutionContextExecutor = system.executionContext
+  val noCertificateCheckContext = ConnectionContext.https(trustfulSslContext)
 
-  def createInsecureSslContext(): SSLContext = {
-    val trustAllCerts: Array[TrustManager] = Array(new X509TrustManager {
-        override def getAcceptedIssuers(): Array[X509Certificate] = null
-        override def checkClientTrusted(certs: Array[X509Certificate], authType: String) = {}
-        override def checkServerTrusted(certs: Array[X509Certificate], authType: String) = {}
-    })
-
-    val sslContext: SSLContext = SSLContext.getInstance("TLS")
-    sslContext.init(null, trustAllCerts, new SecureRandom())
-    sslContext
+  private val trustfulSslContext: SSLContext = {
+    object NoCheckX509TrustManager extends X509TrustManager {
+      override def checkClientTrusted(chain: Array[X509Certificate], authType: String) = ()
+  
+      override def checkServerTrusted(chain: Array[X509Certificate], authType: String) = ()
+  
+      override def getAcceptedIssuers = Array[X509Certificate]()
+    }
+  
+    val context = SSLContext.getInstance("TLS")
+    context.init(Array[KeyManager](), Array(NoCheckX509TrustManager), new SecureRandom())
+    context
   }
-  val noCertificateCheckContext = ConnectionContext.https(createInsecureSslContext)
-
-  implicit val system = ActorSystem()
   val url = "https://self-signed.badssl.com"
-  val response = Await.result(Http().singleRequest(Get(url), noCertificateCheckContext), 60 seconds)
-  println(response)
-  val responseString = Await.result(Unmarshal(response).to[String], 60 seconds)
-  println(responseString)
+  val res = Await.result(Http().singleRequest(Get(url), noCertificateCheckContext), 60 seconds)
+  println(res)
   system.terminate()
 }
--- build.sbt
+++ build.sbt
@@ -1,4 +1,6 @@
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-http" % "10.2.7",
-  "com.typesafe.akka" %% "akka-stream" % "2.6.17"
+  "com.typesafe.akka" %% "akka-actor-typed" % "2.6.17",
+  "com.typesafe.akka" %% "akka-stream" % "2.6.17",
+  "ch.qos.logback" % "logback-classic" % "1.2.10"
 )
